fe275de5499153ee4c4e7d497783d236c5785fa7
Shervin Dehghani <shervin.dehghani@gmail.com>
2020-09-08 11:04:25 +0000

    init

--- shervn-backend/blog/settings.py
+++ shervn-backend/blog/settings.py
@@ -30,11 +30,6 @@ if os.getenv('DJANGO_ENV') == 'prod':
     DEBUG = False
     ALLOWED_HOSTS = ['.shervn.com']
 
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-SECURE_SSL_REDIRECT = True
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
-
 # Application definition
 
 INSTALLED_APPS = [
