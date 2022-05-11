d0f204bc54bf72215065e0a856df3f94dabad8f8
Shervin Dehghani <shervin.dehghani@gmail.com>
2022-05-11 12:34:41 -0400

    update statics

--- shervn-frontend/src/Components/musicComponent.js
+++ shervn-frontend/src/Components/musicComponent.js
@@ -55,15 +55,15 @@ render(){
 return(
   <Container text>
     <br />
-    <p className='nicep post'>Music, and playing music is my [main] hobby. I play different music instruments and always like to try new ones. Piano and Setar (an Iranian music Instrument) are the ones I play for a longer time, but lately I've been practicing Trumpet too.</p>
+    {/* <p className='nicep post'>Music, and playing music is my [main] hobby. I play different music instruments and always like to try new ones. Piano and Setar (an Iranian music Instrument) are the ones I play for a longer time, but lately I've been practicing Trumpet too.</p>
     <p>I record time to time the stuff I play or the sounds I hear. Based on the recordings, I'm gathering two collections which are:</p>
     <ul>
       <li><p>Improvisation for Home: a collection of my improvisations.</p></li>
       <li><p>Through the Cities: a collection of street music of different cities that I have visited.</p></li>
     </ul>
-    <p>They probably lack many quality standards but I still like to continue recording, at least for a while.</p>
-    <Divider />
+    <p>They probably lack many quality standards but I still like to continue recording, at least for a while.</p> */}
     {t}
+    
     <div className="buttons">
           <Button icon='chevron left' onClick=  {  this.prevPage  } />
           <Button icon='chevron right' onClick=  {  this.nextPage  }/>
