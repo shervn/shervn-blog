29ae140460fda0a675f16d649ded051894c106df
Shervin Dehghani <shervin.dehghani@gmail.com>
2024-04-03 12:02:09 +0200

    update image

--- shervn-frontend/src/Components/soundComponent.js
+++ shervn-frontend/src/Components/soundComponent.js
@@ -47,7 +47,7 @@ render(){
       <Container text>
         <Header as='h3' className={element.className}>{element.title}</Header>
         {element.body.split('\n').map(x => <p className={element.className}>{x}</p>)}
-        <iframe className='notinvert' title={element.title} width="100%" height={element.playlist === true ? 350 : 150} scrolling="no" frameborder="no" allow="autoplay" src={element.soundCloudLink}></iframe>
+        <iframe title={element.title} width="100%" height={element.playlist === true ? 350 : 150} scrolling="no" frameborder="no" allow="autoplay" src={element.soundCloudLink}></iframe>
       </Container>
       <Divider />
     </div>
