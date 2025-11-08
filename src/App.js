06aa58920e9ca312ecada28970cbe7a5eb86c53b
Shervin Dehghani <shervin.dehghani@gmail.com>
2025-11-08 23:45:51 +0100

    drop research

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import HeaderText from './Components/headerText'
 import Footer from './Components/footer'
 import Sound from './Components/soundComponent'
 import Blog from './Components/blogComponent'
-import Research from './Components/researchComponent.js'
+// import Research from './Components/researchComponent.js'
 import Reviews from './Components/reviewsComponent.js'
 import Video from './Components/videosComponent.js'
 import Insta from './Components/instaComponent.js'
@@ -51,7 +51,7 @@ render() {
             <Menu.Item as={Link} className="menuFarsi" to={'/noises'}  name="noises" content='noises' active={this.state.activeItem === 'noises'} onClick={this.handleItemClick}/>
             <Menu.Item as={Link} className="menuFarsi" to={'/videos'} name="videos" content='videos' active={this.state.activeItem === 'videos'} onClick={this.handleItemClick}/>
             <Menu.Item as={Link} className="menuFarsi" to={'/reviews'}  name="reviews" content="reviews" active={this.state.activeItem === 'reviews'} onClick={this.handleItemClick} />
-            <Menu.Item as={Link} className="menuFarsi" to={'/research'} name="research" content='research' active={this.state.activeItem === 'research'} onClick={this.handleItemClick}/>
+            {/* <Menu.Item as={Link} className="menuFarsi" to={'/research'} name="research" content='research' active={this.state.activeItem === 'research'} onClick={this.handleItemClick}/> */}
           </Menu>
         </div>
         <Routes>
@@ -60,7 +60,7 @@ render() {
           <Route path='/reviews' element={<Reviews/>} />
           <Route path='/noises' element={<Sound/>} />
           <Route path='/videos' element={<Video/>} />
-          <Route path='/research' element={<Research/>} />
+          {/* <Route path='/research' element={<Research/>} /> */}
           <Route path='/' element={<Blog/>} />
         </Routes>
       </Router>
