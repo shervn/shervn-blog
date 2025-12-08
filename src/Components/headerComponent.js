cf16303fd4f5ae4a9641cce6ce3a2e28be57a419
Shervin Dehghani <shervin.dehghani@gmail.com>
2025-12-08 13:22:46 +0100

    refine

--- src/Components/headerComponent.js
+++ src/Components/headerComponent.js
@@ -1,31 +1,48 @@
-import { useEffect, useState } from 'react'
-import { Header, Image, Modal, Icon, Button } from 'semantic-ui-react'
+import { useEffect, useState } from 'react';
+import { Header, Image, Modal, Icon, Button } from 'semantic-ui-react';
 import { loadData, getImagePath } from '../utils.js';
 
-export default function HeaderComponent(){
-  const [metadata, setMetadata] = useState([]);
+const SPOTIY_RECENT_API = 'https://nibv576k3b.execute-api.us-east-1.amazonaws.com/spotify-latest'
+
+export default function HeaderComponent() {
+  const [metadata, setMetadata] = useState({});
+  const [spotifyTrack, setSpotifyTrack] = useState(null);
   const [modalOpen, setModalOpen] = useState(false);
 
+  // Load general metadata
   useEffect(() => {
     loadData(setMetadata, 'meta');
   }, []);
 
+  useEffect(() => {
+    async function fetchSpotifyTrack() {
+      try {
+        const res = await fetch(SPOTIY_RECENT_API);
+        if (!res.ok) throw new Error('Failed to fetch track');
+        const data = await res.json();
+        setSpotifyTrack(data);
+      } catch (err) {
+        console.error(err);
+        setSpotifyTrack({
+          band: 'Radiohead',
+          song: 'Morning Bell',
+          url: 'https://open.spotify.com/track/4h37RgtBg9iynN3BIL5lFU?si=6d72e93e8ee54b15'
+        });
+      }
+    }
+    fetchSpotifyTrack();
+  }, []);
+
   return (
     <div className="header-container">
+      <Header className='headerText' as='h5' content=''/>
+      <Image className='headerImage' src={getImagePath('blog_cover.png')} />
 
-      <Header className='headerText'
-      as='h5'
-      content=''/>
-      <Image className='headerImage'
-      src={getImagePath('blog_cover.png')}/>
-
-      {/* Music Row */}
-      <div className="music-row">
-        <h5 className='musiclistening'>
-          {metadata.musicprefix}
-          <a href={metadata.musicurl}>{metadata.musictitle}</a> by {metadata.musicartist}
-        </h5>
-      </div>
+      {spotifyTrack && (
+          <h5 className='musiclistening'>
+            ♫ listening to  <a href={spotifyTrack.url}>{spotifyTrack.song}</a> by {spotifyTrack.band}
+          </h5>
+      )}
 
       {/* Modal */}
       <Modal
@@ -46,7 +63,6 @@ export default function HeaderComponent(){
 
       {/* Profile + Text */}
       <div className="profile-row">
-
         <Image
           src={getImagePath('blog_profile.png')}
           id="profilepix"
@@ -69,9 +85,7 @@ export default function HeaderComponent(){
             subheader={metadata.subtitle}
           />
         </div>
-
       </div>
-
     </div>
   );
 };
